8a80f84369878d1d91911e69c72afdca7fb17c36
Harley Prelip <harleyprelip2@gmail.com>
2014-10-08 13:33:17 -0600

    made project
    button changes background color

---
+++ src/com/example/mysillyandroidproject/SillyActivity.java
@@ -0,0 +1,44 @@
+package com.example.mysillyandroidproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class SillyActivity extends Activity
+{
+	private Button appButton;
+	private TextView appText;
+	private RelativeLayout appLayout;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_silly);
+		
+		//gives you access to a button or any view object
+		appButton = (Button) findViewById(R.id.firstButton);
+		appText = (TextView) findViewById(R.id.sillyTextView);
+		appLayout = (RelativeLayout) findViewById(R.id.appLayout);
+		
+		setupListeners();
+	}
+	
+	private void setupListeners()
+	{
+		appButton.setOnClickListener(new View.OnClickListener()
+		{
+			
+			@Override
+			public void onClick(View v)
+			{
+				appLayout.setBackgroundResource(R.color.uglyGreen);
+				
+			}
+		});
+		
+	}
+}
